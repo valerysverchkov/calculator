5a82c3527be850df6e4babb18c904ca651a5539a
Валерий Сверчков <vsverchkov@MacBook-Pro-Valerij.local>
2021-07-14 18:14:50 +0300

    Initial commit

---
+++ src/test/java/ru/gpn/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpn.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
